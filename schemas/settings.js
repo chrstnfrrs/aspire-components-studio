866cc8117d81415fdad7cde8948ab836ba808fd3
Christian Farris <christianfarriscs@gmail.com>
2021-03-01 19:29:04 -0600

    create settings type

---
+++ schemas/settings.js
@@ -0,0 +1,18 @@
+import { MdSettings } from 'react-icons/md'
+
+export default {
+  name: 'settings',
+  type: 'document',
+  fields: [
+    {
+      name: 'navigation',
+      title: 'Navigation',
+      type: 'navigation',
+    },
+  ],
+  preview: {
+    select: {
+      title: 'Settings'
+    }
+  }
+}
