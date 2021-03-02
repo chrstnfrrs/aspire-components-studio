73dbf44390fe8784e9ce36a35e9122638b5fef2f
Christian Farris <christianfarriscs@gmail.com>
2021-03-01 19:28:59 -0600

    create prop type

---
+++ schemas/prop.js
@@ -0,0 +1,23 @@
+import { IoExtensionPuzzleOutline } from 'react-icons/io5'
+
+export default {
+  name: 'prop',
+  title: 'Props',
+  type: 'document',
+  icon: IoExtensionPuzzleOutline,
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'helpers',
+      title: 'Helpers',
+      type: 'reference',
+      to: [{ type: 'helper'}],
+      validation: Rule => Rule.required()
+    }
+  ],
+}
