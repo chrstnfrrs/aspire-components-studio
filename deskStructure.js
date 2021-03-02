377832595eebc509f24b9b1af636b7e1d62f3bb5
Christian Farris <christianfarriscs@gmail.com>
2021-03-01 19:25:22 -0600

    create desk structure

---
+++ deskStructure.js
@@ -0,0 +1,29 @@
+import S from "@sanity/desk-tool/structure-builder";
+import { MdSettings } from 'react-icons/md'
+export default () =>
+  S.list()
+    .title('Content')
+    .items([
+      S.listItem()
+        .title('Application Settings')
+        .icon(MdSettings)
+        .child(
+          S.document()
+            .title('Application Settings')
+            .schemaType('settings')
+            .documentId('appSettings')
+        ),
+      
+      // Add a visual divider (optional)
+      S.divider(),
+      // List out the rest of the document types, but filter out the config type
+      ...S.documentTypeListItems()
+        .filter(listItem => ![
+          'links',
+          'link',
+          'linkOptions',
+          'navigation',
+          'navigationOptions',
+          'settings',
+        ].includes(listItem.getId()))
+    ])
