21b2d2b2b85a70f7fe4de143da12f7093d9d52e8
Christian Farris <christianfarriscs@gmail.com>
2021-03-01 19:28:16 -0600

    create component type

---
+++ schemas/component.js
@@ -0,0 +1,48 @@
+import { IoLogoWebComponent } from 'react-icons/io5'
+
+export default {
+  name: 'component',
+  title: 'Components',
+  type: 'document',
+  icon: IoLogoWebComponent,
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 96
+      },
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'Layout', value: 'layout' },
+          { title: 'Text', value: 'text' },
+          { title: 'Input', value: 'input' },
+          { title: 'Element', value: 'element' },
+          { title: 'Block', value: 'block' },
+          { title: 'Functions', value: 'function' },
+        ]
+      },
+    },
+    {
+      name: 'props',
+      title: 'Props',
+      type: 'reference',
+      to: [{ type: 'prop'}],
+      validation: Rule => Rule.required()
+    }
+  ],
+}
